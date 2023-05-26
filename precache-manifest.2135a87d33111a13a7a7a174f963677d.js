6e4535952e8109c1558c10aa9772b93ba16bc065
BespoyasovaVA <v.markutsaa@mail.ru>
2023-05-26 20:50:43 +0500

    Updates

---
+++ precache-manifest.2135a87d33111a13a7a7a174f963677d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f71f05115b9340497237",
+    "url": "/music/static/css/main.72e0b92a.chunk.css"
+  },
+  {
+    "revision": "f71f05115b9340497237",
+    "url": "/music/static/js/main.f71f0511.chunk.js"
+  },
+  {
+    "revision": "f1aa738279730670c7ab",
+    "url": "/music/static/js/1.f1aa7382.chunk.js"
+  },
+  {
+    "revision": "5277e1b37456f7bd28d8",
+    "url": "/music/static/js/2.5277e1b3.chunk.js"
+  },
+  {
+    "revision": "f1ab4582a1058cdc4f79",
+    "url": "/music/static/js/runtime~main.f1ab4582.js"
+  },
+  {
+    "revision": "913cdb6f6ede19ab7550c5d1efc18d5c",
+    "url": "/music/static/media/Avatar (1).913cdb6f.png"
+  },
+  {
+    "revision": "e4c1324d8916a62010b2de9ef8e48653",
+    "url": "/music/static/media/Avatar (2).e4c1324d.png"
+  },
+  {
+    "revision": "91e0d2f73d3ba5a1b063a2f1178d1c12",
+    "url": "/music/static/media/Avatar (3).91e0d2f7.png"
+  },
+  {
+    "revision": "c88e250fb0dc02f7eded35eb01ff8659",
+    "url": "/music/static/media/timessquarenightwide.c88e250f.mp4"
+  },
+  {
+    "revision": "a7707b5861c5cdb80f9581b32272e8db",
+    "url": "/music/static/media/mucismg.a7707b58.png"
+  },
+  {
+    "revision": "cb1f06a58097bf3b0896f180c139452a",
+    "url": "/music/static/media/music_back.cb1f06a5.png"
+  },
+  {
+    "revision": "479266312f0c8ccb02fc8961a6ee0cc4",
+    "url": "/music/static/media/ltop.47926631.png"
+  },
+  {
+    "revision": "34bcbc95a676336314c8bcdab2a8406d",
+    "url": "/music/static/media/lbottom.34bcbc95.png"
+  },
+  {
+    "revision": "7340599ddc8200e3d45cfebedc952069",
+    "url": "/music/static/media/rtop.7340599d.png"
+  },
+  {
+    "revision": "7278662b261729e9396f773bbb678429",
+    "url": "/music/static/media/rbottom.7278662b.png"
+  },
+  {
+    "revision": "e7b6b75c9237be450eaebdad177ecdf3",
+    "url": "/music/static/media/Img (8).e7b6b75c.png"
+  },
+  {
+    "revision": "80655506a1a08211bb5b4d6dc8c74775",
+    "url": "/music/static/media/Img (9).80655506.png"
+  },
+  {
+    "revision": "fad4baaeaae2c514d2b0ad9ab0da25c9",
+    "url": "/music/static/media/Img (10).fad4baae.png"
+  },
+  {
+    "revision": "350fead20ae66e11a8d4dfc3dea267c3",
+    "url": "/music/static/media/Img (11).350fead2.png"
+  },
+  {
+    "revision": "277776d94fd451a610aad3e542409642",
+    "url": "/music/static/media/Img (12).277776d9.png"
+  },
+  {
+    "revision": "246fdbe488d639c28d5e055b07a13c9e",
+    "url": "/music/index.html"
+  }
+];
